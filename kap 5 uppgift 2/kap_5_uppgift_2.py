1feea5af4b29c4e62977d532e650d2eca5769bb2
MelkerBernhardsson <mebe05001@utb.vaxjo.se>
2022-09-16 08:40:28 +0200

    Add project files.

---
+++ kap 5 uppgift 2/kap_5_uppgift_2.py
@@ -0,0 +1,10 @@
+
+pris=int(input("hur mycket kostar bensin? Pris: "))
+
+if pris <= 10 : print("billig bensin")
+
+elif pris > 10 and pris <= 15 : print("tanka full tank")
+
+elif pris > 15 and pris <= 20 : print("tanka tio liter")
+
+elif pris > 20 : print("bensinen dyr, borja cykla")
